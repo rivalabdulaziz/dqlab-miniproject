531415168d53f4d83337a9f8cfad2c28ceb7c57a
Rival Abdul Aziz <77084080+rivalabdulaziz@users.noreply.github.com>
2022-11-01 11:52:48 +0800

    Update project-sql-3.sql

--- fundamental-sql-3/project-sql-3.sql
+++ fundamental-sql-3/project-sql-3.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT ms.kode_pelanggan,
        ON ms.kode_pelanggan = tr.kode_pelanggan
  WHERE tr.nama_produk IN ('Kotak Pensil DQLab',
                           'Flashdisk DQLab 32 GB',
-                          'Sticky Notes DQLab 500 sheets')
+                          'Sticky Notes DQLab 500 sheets');
 
 /* Output:
 +----------------+--------------------+----------------------------------------+
@@ -19,4 +19,30 @@ SELECT DISTINCT ms.kode_pelanggan,
 | dqlabcust05    | Tommy Sinaga       | Vila Permata Intan Berkilau, Blok A1/2 |
 | dqlabcust07    | Agus Cahyono       | Vila Gunung Seribu, Blok F4 - No. 8    |
 +----------------+--------------------+----------------------------------------+
+*/
+
+/* Penyatuan katalog produk dengan batasan harga tertentu */
+
+SELECT nama_produk,
+       kode_produk, harga 
+  FROM ms_produk_1
+ WHERE harga < 100000
+
+ UNION
+
+SELECT nama_produk,
+       kode_produk, harga 
+  FROM ms_produk_2
+ WHERE harga < 50000
+ 
+/* Output:
++----------------------------+-------------+-------+
+| nama_produk                | kode_produk | harga |
++----------------------------+-------------+-------+
+| Kotak Pensil DQLab         | prod-01     | 62500 |
+| Flashdisk DQLab 64 GB      | prod-02     | 55000 |
+| Flashdisk DQLab 32 GB      | prod-04     | 40000 |
+| Tas Travel Organizer DQLab | prod-07     | 48000 |
+| Gantungan Kunci DQLab      | prod-08     | 15800 |
++----------------------------+-------------+-------+
 */
